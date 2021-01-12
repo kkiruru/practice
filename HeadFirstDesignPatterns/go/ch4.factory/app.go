ff4bc3ffeb81b1fcb4dcf073db7218e718f5d643
허현 <kkiruru@gmail.com>
2021-01-12 09:14:17 +0900

    add package

--- HeadFirstDesignPatterns/go/ch4.factory/app.go
+++ HeadFirstDesignPatterns/go/ch4.factory/app.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	_ "headfirst.design.patterns/factory/ingredient"
 )
 
 func main() {
 	fmt.Print("Hello world")
+}
+
+func init() {
+	fmt.Println("main의 init 함수")
+	log.SetOutput(os.Stdout)
 }
---
+++ HeadFirstDesignPatterns/go/ch4.factory/ingredient/Ingredient.go
@@ -0,0 +1,7 @@
+package ingredient
+
+import "fmt"
+
+func init() {
+	fmt.Println("Ingredient의 init 함수")
+}
