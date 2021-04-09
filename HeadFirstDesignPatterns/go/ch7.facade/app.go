dfaa4742ecdb6e5c4603a07701399ea7e95c81e3
허현 <kkiruru@gmail.com>
2021-04-09 14:37:24 +0900

    [golang] ch7. facade Pattern
    - 테스트 코드 제거

--- HeadFirstDesignPatterns/go/ch7.facade/app.go
+++ HeadFirstDesignPatterns/go/ch7.facade/app.go
@@ -1,35 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	a := GetAmplifierInstance("Top-O-Line Amplifier")
 	t := GetTunerInstance("Top-O-Line AM/FM Tuner", a)
-
 	h := GetHomeTheaterInstance(a, t)
 
 	h.WatchMovie("Raiders of the Lost Ark")
 	h.EndMovie()
-
-	b()
-}
-
-func trace(s string) string {
-	fmt.Println("entering:", s)
-	return s
-}
-
-func un(s string) {
-	fmt.Println("leaving:", s)
-}
-
-func a() {
-	defer un(trace("a"))
-	fmt.Println("in a")
-}
-
-func b() {
-	defer un(trace("b"))
-	fmt.Println("in b")
-	a()
 }
